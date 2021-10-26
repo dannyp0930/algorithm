5f21ca51e233e62af8151c7a69fbe43636297646
dannyp0930 <dannyp0930@gmail.com>
2021-10-26 14:04:15 +0900

    Update baekjoon/1890

--- baekjoon/1890_점프.py
+++ baekjoon/1890_점프.py
@@ -1,30 +1,5 @@
 # DP
 
-# dr = [1, 0]
-# dc = [0, 1]
-#
-#
-# def bfs():
-#     global ans
-#     while Q:
-#         r, c = Q.pop(0)
-#         if r == N - 1 and c == N - 1:
-#             ans += 1
-#             continue
-#         for d in range(2):
-#             nr, nc = r + arr[r][c] * dr[d], c + arr[r][c] * dc[d]
-#             if nr < 0 or nc < 0 or nr >= N or nc >= N: continue
-#             Q.append((nr, nc))
-#
-#
-# N = int(input())
-# arr = [list(map(int, input().split())) for _ in range(N)]
-# dp = [[0] * N for _ in range(N)]
-# Q = [(0, 0)]
-# ans = 0
-# bfs()
-# print(ans)
-
 dr = [1, 0]
 dc = [0, 1]
 
