44e6aed0655c813ac8353a9ad7bf7caeb2a495e3
dannyp0930 <dannyp0930@gmail.com>
2021-11-09 15:12:35 +0900

    Update: DP/SWEA/3307

--- DP/SWEA/3307_최장증가부분수열.py
+++ DP/SWEA/3307_최장증가부분수열.py
@@ -5,8 +5,8 @@ for T in range(1, int(input()) + 1):
     N = int(input())
     arr = list(map(int, input().split()))
     dp = [1] * N
-    for i in range(N):
+    for i in range(1, N):
         for j in range(i):
-            if arr[i] >= arr[j]:
-                dp[i] = max(dp[i], dp[j] + 1)
+            if arr[i] >= arr[j] and dp[i] < dp[j] + 1:
+                dp[i] = dp[j] + 1
     print('#{} {}'.format(T, max(dp)))
