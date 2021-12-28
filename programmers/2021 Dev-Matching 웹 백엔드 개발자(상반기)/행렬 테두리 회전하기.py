c3438283868dd8326256591f7b202e15af5d1961
dannyp0930 <dannyp0930@gmail.com>
2021-12-28 13:07:18 +0900

    Upload: programmers/ 2021 Dev-Matching: 웹 백엔드 개발자(상반기)/행렬 테두리 회전하기

--- programmers/2021 Dev-Matching 웹 백엔드 개발자(상반기)/행렬 테두리 회전하기.py
+++ programmers/2021 Dev-Matching 웹 백엔드 개발자(상반기)/행렬 테두리 회전하기.py
@@ -1,13 +1,28 @@
-# def solution(rows, columns, queries):
-#     answer = []
-#     arr = [[i + 1 + j * rows for i in range(columns)] for j in range(rows)]
-#     for lst in arr:
-#         print(lst)
-#     return answer
+def solution(rows, columns, queries):
+    answer = []
+    arr = [[i + 1 + j * columns for i in range(columns)] for j in range(rows)]
+    for query in queries:
+        r1, c1, r2, c2 = query
+        tmp = arr[r1 - 1][c1 - 1]
+        small = tmp
 
-rows = 4
-columns = 3
-arr = [[i + 1 + j * columns for i in range(columns)] for j in range(rows)]
+        for i in range(r1, r2):
+            arr[i - 1][c1 - 1] = arr[i][c1 - 1]
+            small = min(small, arr[i][c1 - 1])
 
-for lst in arr:
-    print(lst)
+        for i in range(c1, c2):
+            arr[r2 - 1][i - 1] = arr[r2 - 1][i]
+            small = min(small, arr[r2 - 1][i])
+
+        for i in range(r2 - 2, r1 - 2, -1):
+            arr[i + 1][c2 - 1] = arr[i][c2 - 1]
+            small = min(small, arr[i][c2 - 1])
+
+        for i in range(c2 - 2, c1 - 2, -1):
+            arr[r1 - 1][i + 1] = arr[r1 - 1][i]
+            small = min(small, arr[r1 - 1][i])
+
+        arr[r1 - 1][c1] = tmp
+
+        answer.append(small)
+    return answer
