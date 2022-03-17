71b70e7f2f91e7c140825a993d38c96cc3a1c962
dannyp0930 <dannyp0930@gmail.com>
2022-03-17 18:39:20 +0900

    Upload: Math/baekjoon/17425_약수의합

--- Math/baekjoon/17425_약수의합.py
+++ Math/baekjoon/17425_약수의합.py
@@ -1,11 +1,20 @@
 MAX = 1000000
-dp = [0] * (MAX + 1)
+# 약수 합 저장
+dp = [1] * (MAX + 1)
+# 누적 합 저장
+g = [0] * (MAX + 1)
+
+for i in range(2, MAX + 1):
+    j = 1
+    while i * j <= MAX:
+        dp[i * j] += i
+        j += 1
 
 for i in range(1, MAX + 1):
-    for j in range(i, MAX + 1, i):
-        dp[j] += i
-    dp[i] += dp[i - 1]
+    g[i] = g[i - 1] + dp[i]
 
+ans = []
 for _ in range(int(input())):
     N = int(input())
-    print(dp[N])
+    ans.append(g[N])
+print('\n'.join(map(str, ans)))
