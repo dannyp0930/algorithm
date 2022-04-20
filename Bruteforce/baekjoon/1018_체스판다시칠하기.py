2a8f32308881d420c0717693418ffe9ef856cae1
dannyp0930 <dannyp0930@gmail.com>
2022-04-20 20:43:36 +0900

    UDATE: Bruteforce/baekjoon/1018_체스판다시칠하기

--- Bruteforce/baekjoon/1018_체스판다시칠하기.py
+++ Bruteforce/baekjoon/1018_체스판다시칠하기.py
@@ -3,22 +3,19 @@ arr = [input() for _ in range(N)]
 result = N * M
 for i in range(N - 7):
     for j in range(M - 7):
-        flag = (i + j) % 2
-        tmp1 = 0
+        tmp1 = tmp2 = 0
         # (i, j)가 B 기준
         for n in range(i, i + 8):
             for m in range(j, j + 8):
-                if (n + m) % 2 == flag and arr[n][m] == "W":
-                    tmp1 += 1
-                if (n + m) % 2 != flag and arr[n][m] == "B":
-                    tmp1 += 1
-        tmp2 = 0
-        # (i, j)가 B 기준
-        for n in range(i, i + 8):
-            for m in range(j, j + 8):
-                if (n + m) % 2 == flag and arr[n][m] == "B":
-                    tmp2 += 1
-                if (n + m) % 2 != flag and arr[n][m] == "W":
-                    tmp2 += 1
+                if (n + m) % 2 == 0:
+                    if arr[n][m] == "W":
+                        tmp1 += 1
+                    else:
+                        tmp2 += 1
+                else:
+                    if arr[n][m] == "B":
+                        tmp1 += 1
+                    else:
+                        tmp2 += 1
         result = min(tmp1, tmp2, result)
 print(result)
