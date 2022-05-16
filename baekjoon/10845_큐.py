2b9101e5496e16ce18ee99ada758742a0c8b2c51
dannyp0930 <dannyp0930@gmail.com>
2022-05-16 20:20:23 +0900

    UPLOAD: baekjoon/10845_큐

---
+++ baekjoon/10845_큐.py
@@ -0,0 +1,30 @@
+import queue
+import sys
+
+q = []
+for _ in range(int(sys.stdin.readline())):
+    command = list(map(str, sys.stdin.readline().split()))
+    if command[0] == 'push':
+        q.append(int(command[1]))
+    elif command[0] == 'pop':
+        try:
+            print(q.pop(0))
+        except:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(q))
+    elif command[0] == 'empty':
+        if q:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == 'front':
+        try:
+            print(q[0])
+        except:
+            print(-1)
+    else:
+        try:
+            print(q[-1])
+        except:
+            print(-1)
