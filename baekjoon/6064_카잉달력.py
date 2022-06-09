d52062c20327b82ae94bc0e1ea1f91587301bbd3
dannyp0930 <dannyp0930@gmail.com>
2022-06-09 11:28:34 +0900

    UPDATE: baekjoon/6064_카잉달력

--- baekjoon/6064_카잉달력.py
+++ baekjoon/6064_카잉달력.py
@@ -1,11 +1,30 @@
+import sys
+
+
+# 최대 공약수
+def gcd(a, b):
+    if a > b:
+        a, b = b, a
+    while b:
+        a, b = b, a % b
+    return a
+
+
+# 최소 공배수
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+# 카잉 달력
 def kain(m, n, x, y):
-    while x < M * N:
-        if not (x - y) % n:
-            return x
-        x += m
+    for i in range(x, lcm(m, n) + 1, m):
+        tmp = i % n if i % n else n
+        if tmp == y:
+            return i
     return -1
 
 
+input = sys.stdin.readline
 for _ in range(int(input())):
     M, N, X, Y = map(int, input().split())
     print(kain(M, N, X, Y))
