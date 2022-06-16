e553493dcb1a99edad581135d4ad464e80f20301
dannyp0930 <dannyp0930@gmail.com>
2022-06-16 14:32:06 +0900

    UPLOAD: baekjoon/11053_가장증가하는부분수열

---
+++ baekjoon/11053_가장긴증가하는부분수열.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+A = list(map(int, input().split()))
+dp = [1] * N
+for i in range(1, N):
+    for j in range(i):
+        if A[i] > A[j] and dp[i] < dp[j] + 1:
+            dp[i] = dp[j] + 1
+print(max(dp))
