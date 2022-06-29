32779e6798eadae57ed1d67530c7f5bff2a6daf4
dannyp0930 <dannyp0930@gmail.com>
2022-06-29 11:37:04 +0900

    UPDATE: baekjoon/2206_벽부수고이동하기

--- baekjoon/2206_벽부수고이동하기.py
+++ baekjoon/2206_벽부수고이동하기.py
@@ -1,27 +1,32 @@
+import sys
+from collections import deque
+
+
 dr = [1, -1, 0, 0]
 dc = [0, 0, 1, -1]
 
 
 def bfs():
-    while Q:
-        r, c, s = Q.pop(0)
+    q = deque([(0, 0, 0)])
+    visit[0][0][0] = 1
+
+    while q:
+        r, c, w = q.popleft()
         if r == N - 1 and c == M - 1:
-            return visit[r][c][s]
+            return visit[r][c][w]
         for d in range(4):
             nr, nc = r + dr[d], c + dc[d]
-            if 0 <= nr < N and 0 <= nc < M:
-                if arr[nr][nc] == 1 and s == 1:
-                    visit[nr][nc][0] = visit[r][c][1] + 1
-                    Q.append((nr, nc, 0))
-                elif arr[nr][nc] == 0 and visit[nr][nc][s] == 0:
-                    visit[nr][nc][s] = visit[r][c][s] + 1
-                    Q.append((nr, nc, s))
+            if 0 <= nr < N and 0 <= nc < M and visit[nr][nc][w] == 0:
+                if arr[nr][nc] == 0:
+                    q.append((nr, nc, w))
+                    visit[nr][nc][w] = visit[r][c][w] + 1
+                elif w == 0:
+                    q.append((nr, nc, 1))
+                    visit[nr][nc][1] = visit[r][c][w] + 1
     return -1
 
-
+input = sys.stdin.readline
 N, M = map(int, input().split())
-arr = [list(map(int, input())) for _ in range(N)]
+arr = [list(map(int, input().rstrip())) for _ in range(N)]
 visit = [[[0] * 2 for _ in range(M)] for _ in range(N)]
-Q = [(0, 0, 1)]
-visit[0][0][1] = 1
 print(bfs())
