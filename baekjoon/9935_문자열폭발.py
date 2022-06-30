29f49d3b3b49c837260cd7355d433d54732503ae
dannyp0930 <dannyp0930@gmail.com>
2022-06-30 12:54:47 +0900

    UPDATE: 9935_문자열폭발

--- baekjoon/9935_문자열폭발.py
+++ baekjoon/9935_문자열폭발.py
@@ -4,7 +4,7 @@ bomb_size = len(bomb)
 stack = []
 for ch in string:
     stack.append(ch)
-    if ''.join(stack[-bomb_size:]) == bomb:
+    if ch == bomb[-1] and ''.join(stack[-bomb_size:]) == bomb:
         for _ in range(bomb_size):
             stack.pop()
 if not stack:
