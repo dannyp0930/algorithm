da606d42c3376713fbe478675c3a72c6796bc251
dannyp0930 <dannyp0930@gmail.com>
2022-07-20 18:43:09 +0900

    UPDATE: programmers/연습문제/2 x n 타일링

--- programmers/연습문제/2 x n 타일링.py
+++ programmers/연습문제/2 x n 타일링.py
@@ -1,7 +1,6 @@
 def solution(n):
     dp = [0] * (n + 1)
-    dp[1] = 1
-    dp[2] = 2
-    for i in range(3, n + 1):
+    dp[0], dp[1] = 1, 1
+    for i in range(2, n + 1):
         dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007
     return dp[n]
