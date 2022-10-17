8d3166721381f5f46f8c0121de3c02ece68bd4cb
dannyp0930 <dannyp0930@gmail.com>
2022-10-17 13:41:22 +0900

    UPLOAD: baekjoon/1937_욕심쟁이판다

---
+++ baekjoon/1937_욕심쟁이판다.py
@@ -0,0 +1,28 @@
+import sys
+
+
+dr = [1, -1, 0, 0]
+dc = [0, 0, 1, -1]
+
+
+def dfs(r, c):
+    if dp[r][c] == 0:
+        for d in range(4):
+            nr, nc = r + dr[d], c + dc[d]
+            if not (0 <= nr < n and 0 <= nc < n):
+                continue
+            if arr[nr][nc] > arr[r][c]:
+                dp[r][c] = max(dp[r][c], dfs(nr, nc))
+    return dp[r][c] + 1
+
+
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
+ans = 0
+for r in range(n):
+    for c in range(n):
+        ans = max(ans, dfs(r, c))
+print(ans)
