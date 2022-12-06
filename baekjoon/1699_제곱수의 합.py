0faf2a130c9895fa97e16e05d722d5444092876e
dannyp0930 <dannyp0930@gmail.com>
2022-12-06 15:03:13 +0900

    UPLOAD: baekjoon/1699_제곱수의 합

---
+++ baekjoon/1699_제곱수의 합.py
@@ -0,0 +1,9 @@
+MAX = float('inf')
+N = int(input())
+dp = [MAX] * (N + 1)
+dp[0] = 0
+dp[1] = 1
+for i in range(2, N + 1):
+    for j in range(1, int(i ** 0.5) + 1):
+        dp[i] = min(dp[i], dp[i - j ** 2] + 1)
+print(dp[N])
