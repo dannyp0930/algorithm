32c88e56fa402641a6fa17a813871452e9d63f52
dannyp0930 <dannyp0930@gmail.com>
2023-03-02 20:51:31 +0900

    UPLOAD: baekjoon/1495_기타리스트

---
+++ baekjoon/1495_기타리스트.py
@@ -0,0 +1,17 @@
+N, S, M = map(int, input().split())
+V = list(map(int, input().split()))
+dp = [[0] * (M + 1) for _ in range(N + 1)]
+dp[0][S] = 1
+for i in range(N):
+    for j in range(M + 1):
+        if dp[i][j] == 1:
+            if j + V[i] <= M:
+                dp[i + 1][j + V[i]] = 1
+            if j - V[i] >= 0:
+                dp[i + 1][j - V[i]] = 1
+ans = -1
+for s in range(M, -1, -1):
+    if dp[N][s]:
+        ans = s
+        break
+print(ans)
