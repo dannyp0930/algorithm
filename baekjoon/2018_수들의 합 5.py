3175bcc7ed43dbb88b79c6d773c9e8ac0cf1dbe3
dannyp0930 <dannyp0930@onoffmix.com>
2023-03-17 09:14:02 +0900

    UPLOAD: baekjoon/2018_수들의 합 5

---
+++ baekjoon/2018_수들의 합 5.py
@@ -0,0 +1,15 @@
+N = int(input())
+s = e = cnt = tot = 0
+while e <= N:
+    print(s, e)
+    if tot < N:
+        e += 1
+        tot += e
+    elif tot > N:
+        tot -= s
+        s += 1
+    else:
+        cnt += 1
+        e += 1
+        tot += e
+print(cnt)
