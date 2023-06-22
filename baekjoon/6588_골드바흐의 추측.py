72ca15ff616cb4cd21b191b491497b4bbdc88a99
dannyp0930 <dannyp0930@onoffmix.com>
2023-06-22 10:16:26 +0900

    UPDATE: 6588_골드바흐의 추측

--- baekjoon/6588_골드바흐의 추측.py
+++ baekjoon/6588_골드바흐의 추측.py
@@ -1,14 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+
 sieve = [1] * 1000000
 for i in range(2, 1001):
     if sieve[i]:
-        for j in range(i + i, 1000000, i):
-            sieve[j] = 0
-
-while 1:
-    n = int(input())
-    if not n:
-        break
-    for i in range(3, n // 2 + 1):
+        sieve[i + i::i] = [0] * len(sieve[i + i::i])
+while n := int(input()):
+    for i in range(3, n // 2 + 1, 2):
         if sieve[i] and sieve[n - i]:
             print(n, "=", i, "+", n - i)
             break
+    else:
+        print('Goldbach\'s conjecture is wrong.')
