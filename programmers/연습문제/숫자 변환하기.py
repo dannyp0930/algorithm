733c4b016d917f9d3c9fd8a2902450d4b83edb61
dannyp0930 <dannyp0930@gmail.com>
2024-05-02 11:50:21 +0900

    UPLOAD: programmers/연습문제/숫자 변환하기

---
+++ programmers/연습문제/숫자 변환하기.py
@@ -0,0 +1,13 @@
+def solution(x, y, n):
+    dp = [-1] * (y + 1)
+    dp[x] = 0
+    for i in range(x, y + 1):
+        if dp[i] == -1:
+            continue
+        if i + n <= y:
+            dp[i + n] = min(dp[i + n], dp[i] + 1) if dp[i + n] != -1 else dp[i] + 1
+        if i * 2 <= y:
+            dp[i * 2] = min(dp[i * 2], dp[i] + 1) if dp[i * 2] != -1 else dp[i] + 1
+        if i * 3 <= y:
+            dp[i * 3] = min(dp[i * 3], dp[i] + 1) if dp[i * 3] != -1 else dp[i] + 1
+    return dp[-1]
