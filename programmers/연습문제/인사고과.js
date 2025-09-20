e06c2a9075eb662be565f645fa01328e5bea2e9c
dannyp0930 <dannyp0930@gmail.com>
2025-09-21 04:19:55 +0900

    UPDATE: programmers/연습문제/인사고과

--- programmers/연습문제/인사고과.js
+++ programmers/연습문제/인사고과.js
@@ -1,15 +1,17 @@
 function solution(scores) {
     const target = scores[0];
     scores.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]);
-    let answer = 1, maxScore = 0;
-    const sum = target[0] + target[1];
-    scores.forEach((score) => {
-       if (score[1] < maxScore) {
-           if (score === target) return -1;
-       } else {
-           maxScore = maxScore < score[1] ? score[1] : maxScore;
-           if (score[0] + score[1] > sum) answer++;
-       }
-    });
+    let answer = 1, before = 0;
+    for (const score of scores) {
+        if (target[0] < score[0] && target[1] < score[1]) {
+            return -1;
+        };
+        if (before <= score[1]) {
+            if (target[0] + target[1] < score[0] + score[1]) {
+                answer++;
+            }
+            before = score[1];
+        }
+    }
     return answer;
 }
