3c4dd21f7ac1e06f45edd164abc70c28d4e3a10a
dannyp0930 <dannyp0930@gmail.com>
2025-10-13 14:57:36 +0900

    UPLOAD: baekjoon/2841 외계인의 기타 연주

--- baekjoon/2841_외계인의 기타 연주.js
+++ baekjoon/2841_외계인의 기타 연주.js
@@ -7,20 +7,19 @@ const [N, P] = input.shift().split(" ").map(Number);
 const melodies = input.map((x) => x.split(" ").map(Number));
 
 function solution(N, P, melodies) {
-    let answer = 0;
-    const stack = Array.from(Array(P + 1), () => []);
-    for (const [idx, fret] of melodies) {
-        while (stack[idx].length && stack[idx][stack[idx].length - 1] > fret) {
-            stack[idx].pop();
-            answer++;
-        }
-        if (!stack[idx].length || stack[idx][stack[idx].length - 1] < fret) {
-            stack[idx].push(fret)
-            answer++;
-        }
+  let answer = 0;
+  const stack = Array.from(Array(P + 1), () => []);
+  for (const [idx, fret] of melodies) {
+    while (stack[idx].length && stack[idx][stack[idx].length - 1] > fret) {
+      stack[idx].pop();
+      answer++;
     }
-
-    return answer
+    if (!stack[idx].length || stack[idx][stack[idx].length - 1] < fret) {
+      stack[idx].push(fret);
+      answer++;
+    }
+  }
+  return answer;
 }
 
 console.log(solution(N, P, melodies));
