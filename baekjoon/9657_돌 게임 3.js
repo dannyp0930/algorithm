e30360d29da0c2cc0ecd1c192c98bcd186dd43a9
dannyp0930 <dannyp0930@gmail.com>
2025-10-17 01:33:33 +0900

    UPLOAD: baekjoon/9657 돌 게임 3

---
+++ baekjoon/9657_돌 게임 3.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input =
+  process.platform === "linux"
+    ? fs.readFileSync(0, "utf-8").toString().trim()
+    : fs.readFileSync("input.txt").toString().trim();
+
+const N = +input;
+
+function solution(N) {
+  const dp = Array(N + 1).fill(false);
+  dp[1] = true;
+  dp[3] = true;
+  dp[4] = true;
+  for (let i = 5; i <= N; i++) {
+    if (!dp[i - 1] || !dp[i - 3] || !dp[i - 4]) dp[i] = true;
+  }
+  console.log(dp[N] ? "SK" : "CY");
+}
+
+solution(N);
